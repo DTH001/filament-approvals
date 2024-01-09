90cd6f505e0d98d99c49c72d1fdb3c73a23a8083
Dewald Theron <dtheron2@gmail.com>
2024-01-09 16:55:10 +0200

    Revert "Add filament-shield dependency"
    This reverts commit 6e38c02797ada05886ea80dbb96f12f4a3a411e9.

--- src/Models/ApprovableModel.php
+++ src/Models/ApprovableModel.php
@@ -5,13 +5,10 @@ namespace dth001\Approvals\Models;
 use dth001\Approvals\Traits\Approvable;
 use Illuminate\Database\Eloquent\Model;
 use RingleSoft\LaravelProcessApproval\Contracts\ApprovableModel as ContractsApprovableModel;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Spatie\Permission\Traits\HasRoles;
 
-class ApprovableModel extends Authenticatable implements ContractsApprovableModel
+class ApprovableModel extends Model implements ContractsApprovableModel
 {
     use Approvable;
-    use HasRoles;
 
 
 }
