6e38c02797ada05886ea80dbb96f12f4a3a411e9
Dewald Theron <dtheron2@gmail.com>
2024-01-09 16:35:44 +0200

    Add filament-shield dependency

--- src/Models/ApprovableModel.php
+++ src/Models/ApprovableModel.php
@@ -5,10 +5,13 @@ namespace dth001\Approvals\Models;
 use dth001\Approvals\Traits\Approvable;
 use Illuminate\Database\Eloquent\Model;
 use RingleSoft\LaravelProcessApproval\Contracts\ApprovableModel as ContractsApprovableModel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
-class ApprovableModel extends Model implements ContractsApprovableModel
+class ApprovableModel extends Authenticatable implements ContractsApprovableModel
 {
     use Approvable;
+    use HasRoles;
 
 
 }
